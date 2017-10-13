a29e88205c87ebc45f6143b4c51d37ce7a58460d
tamraouisebti <fs_tamraoui@esi.dz>
2017-10-13 14:23:12 +0100

    branche001

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Tamroui Karim on 13/10/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static void launch(String[] args) {
+        /* premier test */
+
+        VectorHelper test=new VectorHelper();
+        test.intvecteur();
+        test.afficherVEc();
+
+    }
+
+
+}
--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -1,8 +1,46 @@
+import java.sql.ResultSet;
+import java.util.Scanner;
+
 /**
  * Created by Tamroui Karim on 11/10/2017.
  */
-public class VectorHelper {
+public class VectorHelper {      /* La classe represente un seul Vecteur*/
 
 
-    void fonction(){};
+    private int vecteur[]; /* Un tablaux des entier (Vecteur)*/
+    private int vlength; /* la Taille du Vecteur*/
+
+
+
+
+
+    public void setVecteur(int V[]){vecteur=V;}
+    public void setVlength(int A){vlength=A;}
+    public int getVlength(){return this.vlength;}
+    public int getcase(int A){return this.vecteur[A];} //retourne la valeur de la case numero A dans le vecteur
+
+    public void intvecteur(){                    /*   initialise les valeur d un vecteur   */
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Donnez la Taille du vecteur ");
+        this.vlength=sc.nextInt();
+        this.vecteur=new int[vlength];
+        int i;
+        for(i=0;i<vlength;i++)
+        {
+           System.out.print(i+":"); vecteur[i]=sc.nextInt();
+        }
+
+    }
+
+    public void afficherVEc()         /* afficher les valeur d un vecteur V*/
+    {   int i;
+    System.out.println();
+        for (i=0;i<vlength;i++){System.out.print("|"+vecteur[i]+"|");}
+    }
+
+
+
+
+
 }
